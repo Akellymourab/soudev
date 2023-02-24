0bc7439bb271ffd4f58f3a75557c3456066bc790
akellymourab <anakellymourab@gmail.com>
2023-02-24 12:05:24 -0300

    add

--- aula 23.02/js/scripts.js
+++ aula 23.02/js/scripts.js
@@ -1,13 +1,34 @@
 function posterior(){
     let itemAtivo = document.querySelector(".active");
-    let posicaoAtiva = itemAtivo.getAttribute("posicao");
-
-    let todosItems = document.querySelectorAll(li);
+    let posicaoAtiva = itemAtivo.getAttribute("posicao");   
+    let todosItems = document.querySelectorAll("li");
+    if(posicaoAtiva == todosItems.length){
+        posicaoAtiva = 1;
+    }else{
+        posicaoAtiva++;
+    }
     for (let i = 0; i < todosItems.length; i++){
-        if(todosItems[i].getAttribute("posicao") == posicaoAtiva + 1){
+        if(todosItems[i].getAttribute("posicao") == posicaoAtiva){
             todosItems[i].classList.add("active")            
         }else{
-            todosItems[i].class.remove("active")
+            todosItems[i].classList.remove("active")
+        }
+    }
+}
+function anterior(){
+    let itemAtivo = document.querySelector(".active");
+    let posicaoAtiva = itemAtivo.getAttribute("posicao");   
+    let todosItems = document.querySelectorAll("li");
+    if(posicaoAtiva == 1){
+        posicaoAtiva = todosItems.length;
+    }else{
+        posicaoAtiva--;
+    }    
+    for (let i = 0; i < todosItems.length; i++){
+        if(todosItems[i].getAttribute("posicao") == posicaoAtiva){
+            todosItems[i].classList.add("active")            
+        }else{
+            todosItems[i].classList.remove("active")
         }
     }
 }
