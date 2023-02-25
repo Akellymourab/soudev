c7f416f42336775b865cf9cee73887649349e850
gabriel47665 <anakellymourab@gamil.com>
2023-02-25 16:27:25 -0300

    add

---
+++ trabalho1/exercicio1/script1.js
@@ -0,0 +1,12 @@
+// function numero(){
+//     let numeroP
+// }
+
+// 
+
+function valorN(){
+    event.preventDefault();
+    let valor = number (numero.value);
+    let resultado = (numero / (mAltura * mAltura)).toFixed(1);
+
+}
