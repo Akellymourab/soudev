95a552181ccfb92d988f0f9a5162293d70a5c29f
Akellymourab <101601082+Akellymourab@users.noreply.github.com>
2023-02-28 09:06:28 -0300

    add aula

---
+++ aula 28.02/eventos.js
@@ -0,0 +1,24 @@
+let contatos = [
+    "mariana",
+    "marilia",
+    "kelly",
+    "lane",
+    "rebeca",
+    "samantha",
+    "carol",
+    "rosana",
+    "andy",
+    "vanessa",
+];
+
+
+for (x = 1; x <= 10; x++){
+    lista.innerHTML += `<li> <strong> ${contatos[x]}: </strong> <strong>85 9 8991-8135</strong></li>`;
+}
+
+// let nome = "lane";
+// nome += "da silva";
+// let cidade = "fortaleza";
+// let idade = 25;
+
+// document.write(`oi, me chamo ${nome}, moro em ${cidade} e tenho ${idade} de idade`)
