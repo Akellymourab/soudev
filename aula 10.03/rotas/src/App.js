1b200ac20328ee22e48dc324e7cdac48d0ffe056
akellymourab <anakellymourab@gamil.com>
2023-03-10 12:56:30 -0300

    add

---
+++ aula 10.03/rotas/src/App.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import Ways from './routes/ways';
+
+export default function app()
+{
+  return(
+    <>
+      <Ways/>
+    </>
+  );
+}
---
+++ aula 10.03/rotas/src/pages/Alunos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../componentes/Header';
+
+export default function Alunos()
+{
+    return(
+        <>
+        <Header/>
+            pagina de alunos 
+        </>
+    )
+}
