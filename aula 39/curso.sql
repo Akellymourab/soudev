7aeec571aa8409ff0647fc35b74aa7925076789b
akellymourab <anakellymourab@gmail.com>
2023-03-27 11:57:30 -0300

    add aula dia 27.03

---
+++ aula 39/curso.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_curso
+    (nome, carga_horaria, descricao)
+VALUES 
+    ('fullstack', '192', 'curso de desenvolvimento web com js');
+
+INSERT INTO tb_curso
+    (nome, carga_horaria, descricao)
+VALUES 
+    ('PHP', '88', 'curso de desenvolvimento web');
+
+INSERT INTO tb_curso
+    (nome, carga_horaria, descricao)
+VALUES 
+    ('marketing digital', '80', 'curso de edição'); 
